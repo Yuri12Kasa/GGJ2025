30069f10605cc3d55893086459d3904e302b882d
Alice Ignirri <99032315+nevischio@users.noreply.github.com>
2025-01-25 21:51:09 +0100

    obstacle spawn

--- GGJ2025/Assets/Scripts/SpawnManager.cs
+++ GGJ2025/Assets/Scripts/SpawnManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
@@ -6,11 +7,22 @@ using Random = UnityEngine.Random;
 public class SpawnManager : MonoBehaviour
 {
     [FormerlySerializedAs("objsToSpawn")] public List<Obstacle> obstaclesToSpawn = new List<Obstacle>();
-    public float spawnStart;
-    public float spawnEnd;
-   
+    public float spawnStart = 1.5f;
+    public float spawnEnd = 19.5f;
+    public float timerToNextSpawn = 1f;
+
+    private float timer;
+    [SerializeField] private Transform _maxSpawnPoint;
+    [SerializeField] private Transform _minSpawnPoint;
+
+    [SerializeField] private float minVerticalPoint;
+    [SerializeField] private float maxVerticalPoint;
     private void Start()
     {
+        timer = 0;
+        minVerticalPoint = _minSpawnPoint.position.y;
+        maxVerticalPoint = _maxSpawnPoint.position.y;
+        
         if (spawnStart < 0)
         {
             spawnStart = 0;
@@ -21,16 +33,27 @@ public class SpawnManager : MonoBehaviour
         }
     }
     private void Update()
-    {
-        if (spawnStart > TimeManager.Instance.SceneTime && spawnEnd<= TimeManager.Instance.SceneTime )//TimeHendler
+    { 
+        if (spawnStart <= TimeManager.Instance.GetTime())
         {
-            Spawn();
+            if (spawnEnd >= TimeManager.Instance.GetTime())
+            {
+                timer += Time.deltaTime;
+                if (timer < timerToNextSpawn) return;
+                Spawn();
+                timer = 0;
+            }
         }
     }
     private void Spawn()
     {
-        var index = Random.Range(0, obstaclesToSpawn.Count);
-        var obstacleToSpawn = obstaclesToSpawn[index];
-        Instantiate(obstacleToSpawn, obstacleToSpawn.transform.position, obstacleToSpawn.transform.rotation);
+        var obstacleToSpawn = obstaclesToSpawn[(int)Randomaizer(0, obstaclesToSpawn.Count)];
+        Instantiate(obstacleToSpawn.gameObject, new Vector3(obstacleToSpawn.transform.position.x,Randomaizer(minVerticalPoint,maxVerticalPoint)), obstacleToSpawn.transform.rotation);
+    }
+
+    private float Randomaizer(float min, float max)
+    {
+        var index = Random.Range(min,max);
+        return index;
     }
 }
